be03316ea7b6ae7f513c201ed3f8f89cf65b080d
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-19 21:22:07 +0530

    delete

--- apps/http-server/src/middlewares/auth.middleware.ts
+++
@@ -1,8 +0,0 @@
-// import type { Request, Response, NextFunction } from "express"
-
-// import AppError from "../utils/AppError.js"
-// import { prisma } from "@repo/db/src/prisma.js"
-// import { verifyAccessToken } from "../utils/token.js"
-
-
-// function CreateAuthMiddleware()
