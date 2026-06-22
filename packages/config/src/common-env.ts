f26c35a53fe6d6b2d289f023969e24ae87e944ca
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-22 22:02:21 +0530

    add - shared env zod validation

---
+++ packages/config/src/common-env.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const ENVSchema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+
+    REDIS_URL: z.string().optional(),
+
+    ACCESS_TOKEN_SECRET: z.string().min(1),
+})
+
+const parsed = ENVSchema.safeParse(process.env);
+
+if (!parsed.success) {
+    console.error("[ERROR] ENV schema validation failed! \n", parsed.error.format());
+    process.exit(1);
+}
+
+export const env = parsed.data;
