4f3d600ffb61a5dcf160341b7e659b1b080a0f93
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-22 15:13:41 +0530

    add invitation_ttl_ms and types

--- packages/config/src/constants.ts
+++ packages/config/src/constants.ts
@@ -1,9 +1,11 @@
-export const ROOM_LIMIT_FREE = 3;
+export const ROOM_LIMIT_FREE: number = 3;
 
-export const MAX_ROOM_MEMBERS = 25;
+export const MAX_ROOM_MEMBERS: number = 25;
 
-export const MAX_CHAT_MESSAGE_LENGTH = 2000;
+export const INVITATION_TTL_MS: number = 3 * 24 * 60 * 60 * 1000;
 
-export const ROOM_CODE_LENGTH = 8;
+export const MAX_CHAT_MESSAGE_LENGTH: number = 2000;
 
-export const DEFAULT_AVATAR = "";
+export const ROOM_CODE_LENGTH: number = 8;
+
+export const DEFAULT_AVATAR: string = "";
