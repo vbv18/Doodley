58b02905b7e9240beb9c7696eeb3465a1adeea98
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-22 15:20:53 +0530

    .

---
+++ apps/http-server/src/modules/auth/auth.types.ts
@@ -0,0 +1,14 @@
+import { GlobalRole } from "@repo/types";
+
+
+export type JWTPayload = {
+    id: number,
+    email: string
+}
+
+export type AuthUser = {
+    id: number;
+    name: string;
+    email: string;
+    globalRole: GlobalRole;
+}
