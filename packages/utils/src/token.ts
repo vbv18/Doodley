a17a659450dc24ee5339124814fd30ecd34933b7
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-27 01:52:14 +0530

    parameterized secrets

--- packages/utils/src/token.ts
+++ packages/utils/src/token.ts
@@ -1,12 +1,7 @@
 import jwt from "jsonwebtoken";
 
-import { env } from "@repo/config";
 import type { JWTPayload } from "@repo/types";
 
-
-export function verifyAccessToken(accessToken: string): JWTPayload {
-    return jwt.verify(
-        accessToken,
-        env.ACCESS_TOKEN_SECRET
-    ) as JWTPayload;
+export function verifyAccessToken(token: string, secret: string): JWTPayload {
+  return jwt.verify(token, secret) as JWTPayload;
 }
