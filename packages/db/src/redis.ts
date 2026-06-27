1900911abc5004a116d379069b8dfd8280e187df
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-27 14:18:30 +0530

    fix - dependencies and add - redis url fault tolerance

--- packages/db/src/redis.ts
+++ packages/db/src/redis.ts
@@ -1,5 +1,9 @@
 import { createClient } from "redis";
 
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is not defined");
+}
+
 export const redis = createClient({
   url: process.env.REDIS_URL,
 });
